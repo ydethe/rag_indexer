9e8a364c28eedc950d6000f7f0aed02c1c7d3fdf
Yann de The <ydethe@gmail.com>
2025-06-18 12:26:14 +0200

    Test GH Action

--- all_tests.sh
+++ all_tests.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-mkdir -p htmldoc/ragindexer
-rm -rf .root
-.venv/bin/pytest
-pdoc --html --force --config latex_math=True -o htmldoc ragindexer
-coverage html -d htmldoc/coverage --rcfile tests/coverage.conf
-coverage xml -o htmldoc/coverage/coverage.xml --rcfile tests/coverage.conf
-docstr-coverage src/ragindexer -miP -sp -is -idel --skip-file-doc --badge=htmldoc/ragindexer/doc_badge.svg
-genbadge coverage -l -i htmldoc/coverage/coverage.xml -o htmldoc/ragindexer/cov_badge.svg
+# mkdir -p htmldoc/ragindexer
+# rm -rf .root
+# .venv/bin/pytest
+.venv/bin/pdoc --html --force --config latex_math=True -o htmldoc ragindexer
+.venv/bin/coverage html -d htmldoc/coverage --rcfile tests/coverage.conf
+.venv/bin/coverage xml -o htmldoc/coverage/coverage.xml --rcfile tests/coverage.conf
+.venv/bin/docstr-coverage src/ragindexer -miP -sp -is -idel --skip-file-doc --badge=htmldoc/ragindexer/doc_badge.svg
+.venv/bin/genbadge coverage -l -i htmldoc/coverage/coverage.xml -o htmldoc/ragindexer/cov_badge.svg
